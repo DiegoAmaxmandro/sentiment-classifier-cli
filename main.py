659c8f3271055bc6b2a4587101f8a16c8c0feb33
DiegoAmaxmandro <102663353+DiegoAmaxmandro@users.noreply.github.com>
2025-05-12 15:21:18 +0100

    Add export to JSON functionality for sentiment results

--- main.py
+++ main.py
@@ -3,21 +3,14 @@ import pandas as pd
 import sys
 from utils import clean_text
 from transformers import pipeline
+import os
 
 # Loading data
 def load_reviews(file_path):
     df = pd.read_csv(file_path)
-    df['cleaned'] = df['review'].apply(clean_text)
-    print('\n Cleaned reviews preview: \n')
-    print(df[['review', 'cleaned']].head())
-    print('\n Loading sentiment classifier \n')
-    classifier = pipeline('sentiment-analysis')
-    print('Classifying sentiment... \n')
-    df['sentiment'] = df['cleaned'].apply(lambda text: classifier(text)[0]['label'])
-    print('\n Classification complete: \n')
-    print(df[['review', 'cleaned', 'sentiment']])
+    print("\n First 5 reviews loaded:\n")
+    print(df.head())
     return df
-
 
 
 
@@ -28,4 +21,26 @@ if __name__ == "__main__":
         sys.exit(1)
 
     input_file = sys.argv[1]
-    load_reviews(input_file)
+    df = load_reviews(input_file)
+
+    # Cleaning text
+    os.makedirs("outputs", exist_ok=True)
+    df['cleaned'] = df['review'].apply(clean_text)
+    print('\n Cleaned reviews preview: \n')
+    print(df[['review', 'cleaned']].head())
+
+    # Classifying text
+    print('\n Loading sentiment classifier \n')
+    classifier = pipeline('sentiment-analysis')
+
+    print('Classifying sentiment... \n')
+    df['sentiment'] = df['cleaned'].apply(lambda text: classifier(text)[0]['label'])
+
+    print('\n Classification complete: \n')
+    print(df[['review', 'cleaned', 'sentiment']])
+
+    # Export to JSON
+    output_path = 'outputs/classified_reviews.json'
+    df.to_json(output_path, orient='records', indent=2)
+    print(f"\n Exported results to {output_path}")
+
